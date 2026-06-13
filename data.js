edeedf55d95a4e6005899826bc2d76c3be70dfcc
hishamparamboor-web <hishamparamboor@gmail.com>
2026-06-14 02:35:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,162 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-coustomer-lounge-pan-11",
+      "name": "COUSTOMER LOUNGE PAN 1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5929396470841644,
+        "pitch": -0.03823605693292009,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0275439280103784,
+          "pitch": 0.010507137234288422,
+          "rotation": 0,
+          "target": "3-working-area-pan-11"
+        },
+        {
+          "yaw": -1.0115849346886563,
+          "pitch": 0.011905912475263847,
+          "rotation": 0,
+          "target": "1-md-canin-pan-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-md-canin-pan-11",
+      "name": "MD CANIN PAN 1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9956202322908014,
+        "pitch": -0.00238118249505348,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9361205107751154,
+          "pitch": 0.08783917648416306,
+          "rotation": 0,
+          "target": "0-coustomer-lounge-pan-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pantry-area-pan-11",
+      "name": "PANTRY AREA PAN 1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.472901874971706,
+        "pitch": 0.028574189940634653,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-working-area-pan-11",
+      "name": "WORKING AREA PAN 1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9225830107772772,
+        "pitch": -0.0024504871128208094,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6580049609326935,
+          "pitch": 0.00714113617787504,
+          "rotation": 0,
+          "target": "0-coustomer-lounge-pan-11"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SYNTISTA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
